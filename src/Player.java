83e58d0ca9e2520dc93a7b80274a0aa8de1b001c
VeryBarry <barry.a.daniels@gmail.com>
2016-09-14 11:32:27 -0400

    Gamey thing

---
+++ src/Player.java
@@ -0,0 +1,40 @@
+/**
+ * Created by VeryBarry on 9/14/16.
+ */
+public class Player {
+    String name;
+    String weapon;
+    String location;
+
+    void chooseName(){
+        System.out.println("What's your name?");
+        name = Game.scanner.nextLine();
+        System.out.println("Welcome, "+ name);
+    }
+    void chooseWeapon() throws Exception {
+        System.out.println("Do you want a lightsaber or a lasergunzz");
+        weapon = Game.scanner.nextLine();
+        if (weapon.equalsIgnoreCase("lightsaber")){
+            System.out.println("Good choice!");
+        }
+        else if (weapon.equalsIgnoreCase("lasergunzz")){
+            System.out.println("That's ok, be a fool.");
+        }
+        else {
+            throw new Exception("ERRRRR! Choose again.");
+        }
+    }
+    void chooseTransport() throws Exception {
+        System.out.println("Wanna take the cruiser or a telly?");
+        location = Game.scanner.nextLine();
+        if (location.equalsIgnoreCase("cruiser")){
+            System.out.println("Fine go the long way.");
+        }
+        else if (location.equalsIgnoreCase("telly")){
+            System.out.println("OK, but not all good things come the fast way.");
+        }
+        else {
+            throw new Exception("Why you gotta be a pain?");
+        }
+    }
+}
